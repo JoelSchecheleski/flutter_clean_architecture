d179c4ad9c509ce9cec6b2c11fb3d45c68e973a7
Joel Schecheleski <joel.jsp@gmail.com>
2023-03-11 20:21:54 -0300

    change Authentication usecase to receive a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,11 +1,15 @@
 import 'package:clean_flutter_app/domain/entities/account_entity.dart';
 
 abstract class Authentication {
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
 
-  Future<AccountEntity> auth({
-    required String email,
-    required String password,
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({
+    required this.email,
+    required this.secret,
   });
-
-
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,8 +1,10 @@
 // import 'dart:io';
-
+import 'dart:async';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
+import 'package:clean_flutter_app/domain/usecases/authentication.dart';
 
 class RemoteAuthentication {
   final HttpClient? httpClient;
@@ -13,13 +15,21 @@ class RemoteAuthentication {
     required this.url,
   });
 
-  Future<void> auth() async {
-    await httpClient?.request(url: url, method: 'post');
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {
+      'email': params.email,
+      'password': params.secret,
+    };
+    await httpClient?.request(url: url, method: 'post', body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({required String url, required String method}) async {}
+  Future<void> request({
+    required String url,
+    required String method,
+    Map? body,
+  }) async {}
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
@@ -36,11 +46,13 @@ void main() {
   });
 
   test('Should call HttpClient with correct values', () async {
-    await sut?.auth();
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
+    await sut?.auth(params);
 
     verify(httpClient?.request(
-      url: url,
-      method: 'post',
-    ));
+        url: url,
+        method: 'post',
+        body: {'email': params.email, 'password': params.secret}));
   });
 }
